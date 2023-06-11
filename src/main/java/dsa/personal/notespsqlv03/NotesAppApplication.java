82e903ab270d69548a7ab476ab135d7bd714e852
EC2 Default User <ec2-user@ip-172-31-9-70.eu-north-1.compute.internal>
2023-06-11 22:26:00 +0000

    build successful

--- src/main/java/dsa/personal/notespsqlv03/NotesAppApplication.java
+++ src/main/java/dsa/personal/notespsqlv03/NotesAppApplication.java
@@ -2,12 +2,20 @@ package dsa.personal.notespsqlv03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NotesAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotesAppApplication.class, args);
 	}
 
+        @RequestMapping("/ping")
+        public String pingApp() {
+                return "Ich hoffe du siehst mich !!";
+        }
 }
