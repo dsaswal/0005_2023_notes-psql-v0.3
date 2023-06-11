10c2960138bdec95502f50d455e56181e8ae3e8f
EC2 Default User <ec2-user@ip-172-31-9-70.eu-north-1.compute.internal>
2023-06-11 22:01:29 +0000

    first commit

---
+++ src/main/java/dsa/personal/notespsqlv03/NotesAppApplication.java
@@ -0,0 +1,13 @@
+package dsa.personal.notespsqlv03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesAppApplication.class, args);
+	}
+
+}
